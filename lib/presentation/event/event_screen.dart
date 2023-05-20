11dabe68dd764236126f250ea3f01690e8b6c401
Franklin Regis <franklin.oliveira@ccc.ufcg.edu.br>
2023-05-20 12:52:00 -0300

    fix: corrige página de eventos

--- lib/presentation/event/event_screen.dart
+++ lib/presentation/event/event_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:beez/constants/app_colors.dart';
 import 'package:beez/presentation/event/create_event_screen.dart';
 import 'package:beez/presentation/navigation/tab_navigation_widget.dart';
@@ -42,151 +44,155 @@ class _EventScreenState extends State<EventScreen> {
           body: Consumer<EventProvider>(
             builder: (_, eventProvider, __) {
               final event = eventProvider.getEvent(widget.id!);
-              return Column(children: [
-                Carousel(isMultipleEvent: false, singleEvent: event),
-                Padding(
-                    padding:
-                        const EdgeInsets.only(top: 12, left: 15, right: 15),
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Text(event.name,
-                              style: Theme.of(context).textTheme.titleLarge),
-                          const SizedBox(height: 10),
-                          RichText(
-                            text: TextSpan(
-                                text:
-                                    "${DateFormat("dd/MM/yyyy").format(event.date.toDate())} - ${event.date.toDate().hour}h - ",
-                                style: Theme.of(context).textTheme.bodyMedium,
-                                children: [
-                                  TextSpan(
-                                      text: event.isFree
-                                          ? "Gratuito"
-                                          : "Evento Pago",
-                                      style: TextStyle(
-                                          color: event.isFree
-                                              ? AppColors.green
-                                              : AppColors.blue))
-                                ]),
-                          ),
-                          const SizedBox(height: 7),
-                          Text(
-                            event.description,
-                            style: Theme.of(context).textTheme.displayMedium,
-                          ),
-                          const SizedBox(height: 7),
-                          Text(
-                            event.address,
-                            style: Theme.of(context).textTheme.headlineMedium,
-                          ),
-                          const SizedBox(height: 7),
-                          Row(children: [
-                            if (event.creator != userProvider.currentUserId)
-                              GestureDetector(
-                                onTap: () {
-                                  if (userProvider.currentUserId == null) {
-                                    AppAlerts.login(alertContext: context);
-                                  } else {
-                                    EventService.toggleLikeEvent(
-                                        event, userProvider.currentUserId!);
-                                  }
-                                },
-                                child: event.interested
-                                        .contains(userProvider.currentUserId)
-                                    ? Row(
+              return SingleChildScrollView(
+                child: Column(children: [
+                  Carousel(isMultipleEvent: false, singleEvent: event),
+                  Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 12, horizontal: 15),
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Text(event.name,
+                                style: Theme.of(context).textTheme.titleLarge),
+                            const SizedBox(height: 10),
+                            RichText(
+                              text: TextSpan(
+                                  text:
+                                      "${DateFormat("dd/MM/yyyy").format(event.date.toDate())} - ${event.date.toDate().hour}h - ",
+                                  style: Theme.of(context).textTheme.bodyMedium,
+                                  children: [
+                                    TextSpan(
+                                        text: event.isFree
+                                            ? "Gratuito"
+                                            : "Evento Pago",
+                                        style: TextStyle(
+                                            color: event.isFree
+                                                ? AppColors.green
+                                                : AppColors.blue))
+                                  ]),
+                            ),
+                            const SizedBox(height: 7),
+                            Text(
+                              event.description,
+                              style: Theme.of(context).textTheme.displayMedium,
+                            ),
+                            const SizedBox(height: 7),
+                            Text(
+                              event.address,
+                              style: Theme.of(context).textTheme.headlineMedium,
+                            ),
+                            const SizedBox(height: 7),
+                            Row(children: [
+                              if (event.creator != userProvider.currentUserId)
+                                GestureDetector(
+                                  onTap: () {
+                                    if (userProvider.currentUserId == null) {
+                                      AppAlerts.login(alertContext: context);
+                                    } else {
+                                      EventService.toggleLikeEvent(
+                                          event, userProvider.currentUserId!);
+                                    }
+                                  },
+                                  child: event.interested
+                                          .contains(userProvider.currentUserId)
+                                      ? Row(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.center,
+                                          children: const [
+                                              Icon(
+                                                Icons.person_pin_circle_rounded,
+                                                size: 30,
+                                                color: AppColors.darkYellow,
+                                              ),
+                                              SizedBox(width: 3),
+                                              Text("Tenho Interesse",
+                                                  style: TextStyle(
+                                                      fontSize: 14,
+                                                      color:
+                                                          AppColors.darkYellow))
+                                            ])
+                                      : Row(children: const [
+                                          Icon(
+                                            Icons.person_pin_circle_outlined,
+                                            size: 30,
+                                            color: AppColors.darkYellow,
+                                          ),
+                                          SizedBox(width: 3),
+                                          Text(
+                                            "Tenho Interesse",
+                                            style: TextStyle(
+                                                fontSize: 14,
+                                                color: AppColors.black),
+                                          )
+                                        ]),
+                                )
+                              else
+                                GestureDetector(
+                                    onTap: () {
+                                      GoRouter.of(context).pushNamed(
+                                          CreateEventScreen.name,
+                                          extra: event);
+                                    },
+                                    child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
                                         crossAxisAlignment:
                                             CrossAxisAlignment.center,
                                         children: const [
-                                            Icon(
-                                              Icons.person_pin_circle_rounded,
-                                              size: 30,
-                                              color: AppColors.darkYellow,
-                                            ),
-                                            SizedBox(width: 3),
-                                            Text("Tenho Interesse",
-                                                style: TextStyle(
-                                                    fontSize: 14,
-                                                    color:
-                                                        AppColors.darkYellow))
-                                          ])
-                                    : Row(children: const [
-                                        Icon(
-                                          Icons.person_pin_circle_outlined,
-                                          size: 30,
-                                          color: AppColors.darkYellow,
-                                        ),
-                                        SizedBox(width: 3),
-                                        Text(
-                                          "Tenho Interesse",
-                                          style: TextStyle(
-                                              fontSize: 14,
-                                              color: AppColors.black),
-                                        )
-                                      ]),
-                              )
-                            else
-                              GestureDetector(
-                                  onTap: () {
-                                    GoRouter.of(context).pushNamed(
-                                        CreateEventScreen.name,
-                                        extra: event);
-                                  },
-                                  child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.center,
-                                      children: const [
-                                        Icon(
-                                          Icons.edit_square,
-                                          size: 23,
-                                          color: AppColors.brown,
-                                        ),
-                                        SizedBox(width: 7),
-                                        Text(
-                                          "Editar Evento",
-                                          style: TextStyle(
-                                              fontSize: 14,
-                                              color: AppColors.brown),
-                                        )
-                                      ])),
-                            Expanded(
-                              child: GestureDetector(
-                                  onTap: () {
-                                    // TODO: DEEP LINK SHARE
-                                  },
-                                  child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.center,
-                                      children: const [
-                                        Icon(
-                                          Icons.share,
-                                          size: 23,
-                                          color: AppColors.black,
-                                        ),
-                                        SizedBox(width: 7),
-                                        Text(
-                                          "Compartilhar",
-                                          style: TextStyle(
-                                              fontSize: 14,
-                                              color: AppColors.black),
-                                        )
-                                      ])),
-                            ),
-                          ]),
-                          const SizedBox(height: 10),
-                          Text("Lista de Interessados",
-                              style: Theme.of(context).textTheme.displayMedium),
-                          const SizedBox(height: 7),
-                          SizedBox(
-                            height: 75,
-                            child: GridView.builder(
+                                          Icon(
+                                            Icons.edit_square,
+                                            size: 23,
+                                            color: AppColors.brown,
+                                          ),
+                                          SizedBox(width: 7),
+                                          Text(
+                                            "Editar Evento",
+                                            style: TextStyle(
+                                                fontSize: 14,
+                                                color: AppColors.brown),
+                                          )
+                                        ])),
+                              Expanded(
+                                child: GestureDetector(
+                                    onTap: () {
+                                      // TODO: DEEP LINK SHARE
+                                    },
+                                    child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: const [
+                                          Icon(
+                                            Icons.share,
+                                            size: 23,
+                                            color: AppColors.black,
+                                          ),
+                                          SizedBox(width: 7),
+                                          Text(
+                                            "Compartilhar",
+                                            style: TextStyle(
+                                                fontSize: 14,
+                                                color: AppColors.black),
+                                          )
+                                        ])),
+                              ),
+                            ]),
+                            const SizedBox(height: 10),
+                            Text("Lista de Interessados",
+                                style:
+                                    Theme.of(context).textTheme.displayMedium),
+                            const SizedBox(height: 7),
+                            GridView.builder(
+                                shrinkWrap: true,
+                                physics: const NeverScrollableScrollPhysics(),
                                 gridDelegate:
                                     const SliverGridDelegateWithFixedCrossAxisCount(
                                   mainAxisExtent: 40,
-                                  mainAxisSpacing: 10,
+                                  mainAxisSpacing: 15,
+                                  crossAxisSpacing: 10,
                                   crossAxisCount: 2,
                                 ),
                                 itemCount: event.interested.length,
@@ -198,10 +204,10 @@ class _EventScreenState extends State<EventScreen> {
                                             user: userProvider.getUser(
                                                 event.interested[index]))
                                       ]);
-                                }),
-                          )
-                        ]))
-              ]);
+                                })
+                          ]))
+                ]),
+              );
             },
           ),
           bottomNavigationBar: const TabNavigation(),
